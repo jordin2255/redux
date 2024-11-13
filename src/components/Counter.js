3f15df808f87c5746c02d20b8fbfcc193679f8cf
jordin <jordinmathew.mec@gmail.com>
2024-11-14 09:34:01 +1000

    authentication header component implemented

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,28 @@
 import { useSelector, useDispatch } from 'react-redux';
+
+import { counterActions } from '../store';
 import classes from './Counter.module.css';
 
 
 const Counter = () => {
   const dispatch=useDispatch();
-  const counter=useSelector(state=>state.counter);
-  const show=useSelector(state=>state.showCounter)
-  const incrementHandler=()=>{dispatch({type:'increment'});};
-  const decrementHandler=()=>{dispatch({type:'decrement'});};
+  const counter=useSelector(state=>state.counter.counter);
+  const show=useSelector(state=>state.counter.showCounter);
 
-  const toggleCounterHandler = () => {dispatch({type:'toggle'})};
+  const increaseHandler=()=>{dispatch(counterActions.increment());};
+  const decreaseHandler=()=>{dispatch(counterActions.decrement())}
+  // const incrementHandler=()=>{dispatch({type:'increment'});};
+  // const decrementHandler=()=>{dispatch({type:'decrement'});};
+
+  const toggleCounterHandler = () => {dispatch(counterActions.toggleCounter())};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler}>increment</button>
-        <button onClick={decrementHandler}>decrement</button>
+        <button onClick={increaseHandler}>increment</button>
+        <button onClick={decreaseHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,60 @@
-import {createStore} from 'redux';
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer=(state={counter:0,showCounter:true},action)=>{
-  if(action.type==='increment'){
-    return{
-      counter:state.counter+1,
-      showCounter:state.showCounter,
-    }
-  }
-    if(action.type==='decrement'){
-      return{
-        counter:state.counter-1,
-        showCounter:state.showCounter,
-      }
-    }
-    if(action.type==='toggle'){
-      return{
-        counter:state.counter,
-        showCounter:!state.showCounter,
-      }
-    }
-    return state;
-  }
+const initialCounterState = { counter: 0, showCounter: true };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState:initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+const initialAuthState = { isAuthenticated: false };
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+// const counterReducer=(state=initialState,action)=>{
+//   if(action.type==='increment'){
+//     return{
+//       counter:state.counter+1,
+//       showCounter:state.showCounter,
+//     }
+//   }
+//     if(action.type==='decrement'){
+//       return{
+//         counter:state.counter-1,
+//         showCounter:state.showCounter,
+//       }
+//     }
+//     if(action.type==='toggle'){
+//       return{
+//         counter:state.counter,
+//         showCounter:!state.showCounter,
+//       }
+//     }
+//     return state;
+//   }
 
-const store=createStore(counterReducer);
+const store = configureStore({
+  reducer: {counter:counterSlice.reducer ,auth:authSlice.reducer}
+});
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
-
-
